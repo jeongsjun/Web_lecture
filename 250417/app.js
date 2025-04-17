1ac802ee675dbe32f72f5935f8ed796fda24ad22
seungjunJeong <tmdwns7429@gmail.com>
2025-04-17 09:03:18 +0900

    기본 구조 코드 주석 추가
    기본 구조 코드 주석 추가

---
+++ 250417/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+var app = express();
+const server = http.createServer(app).listen(80);
+
+app.get('/test', function (req, res) {
+  res.send("hello world");
+});
+
+app.get('/main', function (req, res) {
+  res.sendfile("main.html");
+});
+
+console.log("backend") // 터미널에 찍힘
