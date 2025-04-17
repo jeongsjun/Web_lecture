b3b86c4ef2b1db0332eab48858be8659749e060f
seungjunJeong <tmdwns7429@gmail.com>
2025-04-17 17:01:05 +0900

    250417 수업 및 결과 정리

--- 250306/app.js
+++ 250306/app.js
@@ -1,20 +1,24 @@
-const express = require('express');
+// app.js 백엔드
+
+// 서버를 만들기 위해서 가져다 쓰는 코드
+const express = require('express'); //express라는 변수에 express를 가져다 쓰겠다.
 const http = require('http');
 var app = express();
-const server = http.createServer(app).listen(80);
+const server = http.createServer(app).listen(80); // 80포트로 요청과 응답을 받는다.
 
-app.get('/test', function (req, res) {
-  res.send("hello world");
+// 요청과 응답 코드
+app.get('/test', function (req, res) { // 'test'라는 요청이 오면
+  res.send("hello world"); // 'hello world'라는 텍스트를 응답으로 보내줘라
 });
 
-app.get('/test2', function (req, res) {
-  res.send("hello world2");
+app.get('/test2', function (req, res) { // 'test2'라는 요청이 오면
+  res.send("hello world2"); // 'hello world2'라는 텍스트를 응답으로 보내줘라
 });
 
-app.get('/test3', function (req, res) {
-  res.send("hello world3");
+app.get('/test3', function (req, res) { // 'test3'라는 요청이 오면
+  res.send("hello world3");  // 'hello world3'라는 텍스트를 응답으로 보내줘라
 });
 
-app.get('/', function (req, res) {
-  res.sendfile("main.html");
+app.get('/', function (req, res) { // 'main'이라는 요청이 오면
+  res.sendfile("main.html"); // 'main.html'파일을 응답으로 보냄
 });
--- 250417/app.js
+++ 250417/app.js
@@ -11,4 +11,29 @@ app.get('/main', function (req, res) {
   res.sendfile("main.html");
 });
 
+app.get('/array', function (req, res) {
+  res.sendfile("array.html");
+});
+
+app.get('/arrayEx1', function (req, res) {
+  res.sendfile("arrayEx1.html");
+});
+
+app.get('/arrayEx2', function (req, res) {
+  res.sendfile("arrayEx2.html");
+});
+
+app.get('/arrayEx3', function (req, res) {
+  res.sendfile("arrayEx3.html");
+});
+
+app.get('/radio', function (req, res) {
+  res.sendfile("radio.html");
+});
+
+app.get('/radioEx1', function (req, res) {
+  res.sendfile("radioEx1.html");
+});
+
+
 console.log("backend") // 터미널에 찍힘
